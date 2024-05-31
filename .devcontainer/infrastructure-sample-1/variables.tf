d7533cfca09fff691dfab93de893d6f5a9af989a
kennyr3221 <kennyr3221@outlook.com>
2024-05-31 12:54:45 -0400

    add: var file

---
+++ .devcontainer/infrastructure-sample-1/variables.tf
@@ -0,0 +1,13 @@
+variable "global_settings" {
+  description = "Global settings for the deployment"
+  type = object({
+    prefixes      = optional(list(string), null)
+    suffixes      = optional(list(string), null)
+    random_length = optional(number, null)
+  })
+  default = {
+    prefixes      = ["dev"]
+    suffixes      = ["001"]
+    random_length = 4
+  }
+}
