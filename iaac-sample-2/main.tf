42b21dd282a3130bf40da81350bca80adeb64959
Kenny <146675877+kennyr3221@users.noreply.github.com>
2024-06-01 17:14:38 -0400

    chore:add providers

--- iaac-sample-2/main.tf
+++ iaac-sample-2/main.tf
@@ -1,5 +1,18 @@
 provider "azurerm" {
   features {}
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
-
+resource "azurerm_resource_group" "project1_rg" {
+  name     = "project1-resource-group"
+  location = "East US"
+}
