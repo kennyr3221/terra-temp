ad92b87c9b63749cff23ce9072a3323780af081c
kennyr3221 <kennyr3221@outlook.com>
2024-06-03 03:13:20 -0400

    chore: adding vnet and subnet

--- iaac-sample-2/main.tf
+++ iaac-sample-2/main.tf
@@ -3,16 +3,15 @@ provider "azurerm" {
 }
 
 terraform {
-  required_version = ">= 1.8.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     azurerm = {
-      source  = "hashicteorp/azurerm"
-      version = "~> 3.106.1"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
 }
-
 resource "azurerm_resource_group" "project1_rg" {
   name     = var.resource_group_name
   location = var.location
